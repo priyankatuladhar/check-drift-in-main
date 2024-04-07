c2c01009a1db03687799e2df339a118493827e83
Priyanka Tuladhar <136885106+priyankatuladhar@users.noreply.github.com>
2024-04-07 21:21:42 +0545

    Update main.tf

--- modules/s3-lock/main.tf
+++ modules/s3-lock/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "pri-terraform-state" {
-  bucket = "pri-terraform-state-07-07"
+  bucket = "pri-terraform-state-07-07-new"
   # Tags
   tags = merge(
     var.tags,
