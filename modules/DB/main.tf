a8beee7b8a0be211b68865af212283f3d68168aa
Priyanka Tuladhar <136885106+priyankatuladhar@users.noreply.github.com>
2024-04-07 21:36:43 +0545

    Update main.tf

--- modules/DB/main.tf
+++ modules/DB/main.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "terraform-lock" {
-  name           = "pri_terraform_state-0707-new-0707"
+  name           = "pri_terraform_state-0707-new-07-new"
   read_capacity  = 5
   write_capacity = 5
   hash_key       = "LockID"
